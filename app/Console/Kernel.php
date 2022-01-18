50a058a31c4456b4d603b8a4e9dba61b2195b36e
andrew <asternov97@yandex.ru>
2022-01-18 18:57:14 +0530

    commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('rocket')->everyMinute();
+         $schedule->command('rocket')->dailyAt('5:00');
     }
 
     /**
